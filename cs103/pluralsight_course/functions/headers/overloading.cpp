8c9126d261b31903b313562830b49578c026255e
Shantanu Jhaveri <sj06434@usc.edu>
2020-05-15 23:33:35 -0700

    overloading.cpp

---
+++ cs103/pluralsight_course/functions/headers/overloading.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+double add(double x, double y){
+    return x+y;
+}
+
+double add(double a, double b, double c){
+    return a + b + c;
+}
+
+bool test (bool x){
+    return x;
+}
+
+bool test (double x){
+    return x>0;
+}
+
+int main(){
+    auto a = add(3,4);
+    cout << "3+4 is " << a << endl;
+    double b = add(1.2,3.4);
+    cout << "1.2+3.4 is " << b << endl;
+    auto c = add(1.1,2.2,3.3);
+    cout << "1.1+2.2+3.3 is " << c << endl;
+
+    if (test(true)){
+        cout << "true passes the test" << endl;
+    }
+    if (test(3.2)){
+        cout << "3.2 passes the test" << endl;
+    }
+    return 0;
+}
