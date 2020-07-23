c8251ef9e1af5998d1800c7d6d4201c225fa91fe
Shantanu Jhaveri <sj06434@usc.edu>
2020-07-22 19:21:15 -0700

    more rando stuff

---
+++ pluralsight_coursecpp17/ClassesAndObjects.cpp
@@ -0,0 +1,11 @@
+#include "Person.h"
+using namespace std;
+
+int main()
+{
+    Person p1;
+    Person p2;
+    string name = p1.getName();
+
+    return 0;
+}
---
+++ pluralsight_coursecpp17/Person.h
@@ -0,0 +1,11 @@
+#include <string>
+class Person 
+{
+    private:
+        std:: string firstName;
+        std:: string lastName;
+        int arbitaryNumber;
+    
+    public:
+        std::string getName();
+};
